4734eef29c72ec9e24280a9c1ad60833519bb782
Joao_Paulo_Ramos_Vieira <jpramos.comp@gmail.com>
2022-10-15 12:07:46 -0300

    altered anagram

--- anagram.js
+++ anagram.js
@@ -1,41 +1,33 @@
-function buildCharMap(str){
-  
+function buildCharMap(str) {
   let charMap = {};
-
-  for (let char of str.replace(/[^\w]/g, '').toLowerCase()){
-    
-    if(charMap[char]){
-      charMap[char]++
-    }else{
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
       charMap[char] = 1;
     }
   }
 
-  console.log(charMap)
-  
+  console.log(charMap);
+
   return charMap;
-  
 }
 
-function anagram(stringA, stringB){
+function anagram(stringA, stringB) {
+  let strA = buildCharMap(stringA);
+  let strB = buildCharMap(stringB);
 
-  let strA = buildCharMap(stringA)
-  let strB = buildCharMap(stringB)
-
-  if(Object.keys(strA).length !== Object.keys(strB).length){
-          return false;
+  if (Object.keys(strA).length !== Object.keys(strB).length) {
+    return false;
   }
 
-  for(let char in strA){
-
-    if(strA[char]!==strB[char]){
+  for (let char in strA) {
+    if (strA[char] !== strB[char]) {
       return false;
     }
   }
 
   return true;
-
 }
 
-console.log(anagram('joao   ', 'a joo'))
-
+console.log(anagram("joao   ", "a joo"));
