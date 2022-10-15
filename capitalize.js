120424b91ca05262657a3a7a4ce14e104d534c7f
Joao_Paulo_Ramos_Vieira <jpramos.comp@gmail.com>
2022-10-15 12:08:26 -0300

    altered capitalize

--- capitalize.js
+++ capitalize.js
@@ -1,30 +1,25 @@
-
-function captalize(str){
-
+function captalize(str) {
   const words = [];
 
-  for(let word of str.split(' ')){
+  for (let word of str.split(" ")) {
     words.push(word[0].toUpperCase() + word.slice(1));
   }
 
-  return console.log(words.join(' '));
-
+  return console.log(words.join(" "));
 }
 
-function captalizeTwo(str){
-  
+function captalizeTwo(str) {
   let result = str[0].toUpperCase();
 
-  for(let i = 1; i< str.length; i++){
-
-    if(str[i -1]===' '){
-      result+= str[i].toUpperCase();
-    }else{
-      result+=str[i]
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
     }
   }
 
-  return console.log(result)
+  return console.log(result);
 }
 
-captalizeTwo('joao ramos vieira')
+captalizeTwo("joao ramos vieira");
