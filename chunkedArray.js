0a4a0b8e44d9b5e689bb7df87efc97f2e19255ec
Joao_Paulo_Ramos_Vieira <jpramos.comp@gmail.com>
2022-10-15 12:09:54 -0300

    chunke arrary altered

--- chunkedArray.js
+++ chunkedArray.js
@@ -1,19 +1,15 @@
-
-function chucked(arr, size){
+function chucked(arr, size) {
   let chunked = [];
 
-  for(let element of arr){
-    const last = chunked[chunked.length-1]
+  for (let element of arr) {
+    const last = chunked[chunked.length - 1];
 
-    if(!last || last.length===size){
-      chunked.push([element])
-    }
-    else 
-      last.push(element)
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else last.push(element);
   }
 
-  return console.log(chunked)
-
+  return console.log(chunked);
 }
 
-chucked([1,2,3,5,6,7,8], 3)
+chucked([1, 2, 3, 5, 6, 7, 8], 3);
