7e1abd518e72582cf985a56240fb8bb6941babcc
Joao_Paulo_Ramos_Vieira <jpramos.comp@gmail.com>
2022-10-15 12:09:12 -0300

    altered maxChars

--- maxChars.js
+++ maxChars.js
@@ -1,24 +1,22 @@
+let str = "aaaacccbb2";
 
-let str = "aaaacccbb2"
-
-function maxChars(str){
-  let chars={}
-let max = 0;
-let maxChar = ''
-for(let char of str){
-if(chars[char]){
-  chars[char]++
-}else{
-  chars[char] = 1;
-}
-}
-for(let char in chars){
-  if(chars[char]>max){
-    max = chars[char];
-    maxChar = char
+function maxChars(str) {
+  let chars = {};
+  let max = 0;
+  let maxChar = "";
+  for (let char of str) {
+    if (chars[char]) {
+      chars[char]++;
+    } else {
+      chars[char] = 1;
+    }
   }
+  for (let char in chars) {
+    if (chars[char] > max) {
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
 }
-return maxChar
-}
-console.log(maxChars("aabbbcc"))
-
+console.log(maxChars("aabbbcc"));
