e8b15ef18f8010afe5a57d407058311ce6d6c176
Joao_Paulo_Ramos_Vieira <jpramos.comp@gmail.com>
2022-10-20 22:02:56 -0300

    updated reverseString algo

--- reversestring.js
+++ reversestring.js
@@ -1,50 +1,34 @@
-
-
-function reverseSolOne(str){
-
-return str
-  .split('')
-  .reverse()
-  .join('')
+function reverseSolOne(str) {
+  return str.split("").reverse().join("");
 }
 
-
-function reverseSolTwo(str){
-let reversed = '';
-debugger;
-for (let char of str){
-
-   reversed = char + reversed;
+function reverseSolTwo(str) {
+  let reversed = "";
+  debugger;
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
 }
-return reversed;
 
-  }
+function reverseSolTrhee(str) {
+  debugger;
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
 
-
-  function reverseSolTrhee(str){
-    debugger;
-    return str.split('').reduce((rev, char)=>char+rev,'')
-    
-  }
-
-  
-  function fizzbuzz(n){
-
-    for(let i = 1; i<=n; i++){
+function fizzbuzz(n) {
+  for (let i = 1; i <= n; i++) {
     //Is the number a multiple of 3 and 5?
-    if(i % 15 ===0){
-      console.log('FizzBUzz')
-    }else if(i % 3===0){
-      console.log('Fizz')
-    }else if(i % 5===0){
-      console.log('Buzz')
-    }else{
-      console.log(i)
+    if (i % 15 === 0) {
+      console.log("FizzBUzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
     }
-    
   }
-  }
+}
 
-  fizzbuzz(15)
-
-  
+fizzbuzz(15);
